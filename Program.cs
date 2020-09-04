6ed36e6a58a29d7d42ce45719fcb9f4ae0016d11
irockmywarlock <noemirsevilla@gmail.com>
2020-09-03 21:46:46 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> mixedList = new List<object>();
+            int[] integers = { 7, 28, -1 };
+            mixedList.Add(7);
+            mixedList.Add(28);
+            mixedList.Add(-1);
+            mixedList.Add(true);
+            mixedList.Add("chair");
+            var sum = 0;
+            for (var i = 0; i < mixedList.Count; i++)
+            {
+                Console.WriteLine(mixedList[i]);
+                if (mixedList[i] is int)
+                {
+                    sum += (int)mixedList[i];
+                }
+            }
+        Console.WriteLine(sum);
+        }
+    }
+}
